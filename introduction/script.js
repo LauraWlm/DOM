f23d37b5a81e6b6bf4bbd532f474ee31f25e8b89
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-29 11:59:55 +0100

    modif

--- introduction/script.js
+++ introduction/script.js
@@ -1,6 +1,7 @@
 console.log(document.title);
 document.title = "Modifying the DOM";
 document.body.style.backgroundColor = 'rgb(241, 226, 190)';
+/*document.body.style.backgroundColor = '#FF69B4';*/
 
 for (let node of document.body.childNodes) {
     console.log(node)
