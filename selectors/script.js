27fdbb4a9e784b6a63e2c56a3eba03566c80a9da
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-29 14:38:17 +0100

    all

--- selectors/script.js
+++ selectors/script.js
@@ -1,1 +1,30 @@
-const class 
+const importantElement = document.querySelectorAll('.important');
+
+importantElement.forEach(element => {
+    element.setAttribute('title', 'this in an important item');
+});
+
+
+const img= document.querySelectorAll("img");
+
+images.forEach(img => {
+    if (!img.classList.contains("important")) {
+        img.style.display = "none";
+    }
+});
+
+const paragraphs = document.querySelectorAll("p");
+
+paragraphs.forEach(p => {
+    console.log(p.textContent);
+    if (p.className) {
+        console.log("Class name: " + p.className);
+    }
+});
+
+paragraphs.forEach(paragraph => {
+    if (!paragraph.classList.contains("special")) {
+      let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+      paragraph.style.color = randomColor;
+    }
+  });
