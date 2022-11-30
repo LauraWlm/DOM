8a8f8297af64b85691416e1319f692544b575171
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-30 09:26:11 +0100

    all

--- selectors/script.js
+++ selectors/script.js
@@ -5,26 +5,24 @@ importantElement.forEach(element => {
 });
 
 
-const img= document.querySelectorAll("img");
-
+const img = document.querySelectorAll('img');
 img.forEach(img => {
-    if (!img.classList.contains("important")) {
-        img.style.display = "none";
+    if (!img.classList.contains('important')) {
+        img.style.display = 'none';
     }
 });
 
-const paragraphs = document.querySelectorAll("p");
-
+const paragraphs = document.querySelectorAll('p');
 paragraphs.forEach(p => {
     console.log(p.textContent);
     if (p.className) {
-        console.log("Class name: " + p.className);
+        console.log('Class name: ' + p.className);
     }
 });
 
 paragraphs.forEach(paragraph => {
-    if (!paragraph.classList.contains("special")) {
-      let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    if (!paragraph.classList.contains('special')) {
+      let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
       paragraph.style.color = randomColor;
     }
   });
