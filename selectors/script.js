b3e5142bb3ed74378c63a3a4744bbfce20a0497e
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-11-30 13:52:16 +0100

    all

--- selectors/script.js
+++ selectors/script.js
@@ -1,20 +1,24 @@
-let importantElement = document.querySelectorAll('.important');
-for (let element of importantElement) 
-    element.setAttribute('title', 'this in an important item');
 
+function randomColor(x)
+{
+    return Math.floor(Math.random()*x);
+}
 
-let img_modif = document.querySelectorAll('img');
-for (let element of img_modif)
-     if(!element.classList.contains('important'))
-         element.style.display ="none;";
-    
-let parag = document.querySelectorAll('p');
+const importantElement = document.querySelectorAll('.important');
+for (let element of importantElement)
+element.setAttribute('title',"This is an important item");
+
+const img = document.querySelectorAll('img');
+for (let element of img)
+    if (!element.classList.contains('important'))
+        element.style.display = "none";
+
+const parag = document.querySelectorAll('p');
 for (let element of parag)
 {
     if (element.getAttribute('class'))
-        console.log("class: " );
-        
+        console.log("Class: " + element.getAttribute('class'));
     else
-        element.style.background
+        element.style.background = "rgb(" + randomColor(255) + ", " + randomColor(255) + ", " + randomColor(255) + ")";
+    console.log(element.textContent);
 }
-
