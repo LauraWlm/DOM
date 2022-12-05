853d39383d782c7975231c464b22498f28aa92d2
LauraWlm <laurawilhelmi.pro@gmail.com>
2022-12-05 14:10:31 +0100

    all

--- 6.events/script.js
+++ 6.events/script.js
@@ -4,7 +4,7 @@ const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
-const clickOnSquare = (e) => {
+const clickOnSquare = (e) => {  
   console.log(e.target.classList[1])
   console.log(getElapsedTime())
 }
@@ -14,3 +14,12 @@ for (let actionSquare of actionSquares) {
   actionSquare.addEventListener('click', clickOnSquare)
 }
 
+
+e.createElement(div),
+function displayAnAlert(){
+  return alert('The button shall not be pressed!')
+}
+e.addEventListener(
+'click', // we listen to the click event
+displayAnAlert // We trigger the callback function
+)
